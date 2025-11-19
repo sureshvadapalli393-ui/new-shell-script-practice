c9c4f193c04d94c7fa7797970c31af575b3bc6cb
suresh vaadapalli <surehs@gmail.com>
2025-11-19 23:02:06 +0530

    messgae

--- installation.sh
+++ installation.sh
@@ -3,14 +3,14 @@
 SUDO_ID=$(id -u)
 
 if [ $SUDO_ID -eq 0 ]; then
- 
+
 echo "Sudo user"
 exit 1
 fi
 
 dnf install mysql -y
 
-if[ $? -eq 0 ]; then
+if[ $? -ne 0 ]; then
 echo "no acces"
 exit 1
 else 
