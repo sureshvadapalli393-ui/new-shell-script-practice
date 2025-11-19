aa9217a03b5441e39b1e7e82ec97576b44f57977
suresh vaadapalli <surehs@gmail.com>
2025-11-19 22:32:12 +0530

    messgae

--- conditions.sh
+++ conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
 echo "number is less than 10"
-elif [$NUMBER -eq 10]; then
+elif [ $NUMBER -eq 10 ]; then
 echo "number is eqal to given number"
 else 
 echo "nmber is eqal or greater than 10"
