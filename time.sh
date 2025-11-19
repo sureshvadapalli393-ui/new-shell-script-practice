5802f013115f9d064ad9c22a04fbbb703592fbaf
suresh vaadapalli <surehs@gmail.com>
2025-11-19 20:59:49 +0530

    read

--- time.sh
+++ time.sh
@@ -2,8 +2,8 @@
 
 START_DATE=$(date +%s)
 
-sleep 20 &
+sleep 20 
 
 END_TIME=$(date +%s)
 
-echo "script executed time taken: (($END_TIME-$START_DATE))"
+echo "script executed time taken: ($(END_TIME-START_DATE))"
