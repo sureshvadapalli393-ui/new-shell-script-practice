3aaecd1858513258ff011e543c84751d831877dd
suresh vaadapalli <surehs@gmail.com>
2025-11-19 21:18:20 +0530

    read

--- special.sh
+++ special.sh
@@ -3,3 +3,7 @@ echo "All variables passed to the script: $@"
 echo "all variables passed to the script : $*"
 echo "script name : $0"
 echo "current working directory: $PWD"
+
+
+NAME=devops
+echo $NAME
