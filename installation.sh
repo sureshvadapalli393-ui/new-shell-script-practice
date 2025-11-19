e944f3e32e81c52dd1f66a7e120b0f12fc1eab72
suresh vaadapalli <surehs@gmail.com>
2025-11-19 23:11:33 +0530

    messgae

--- installation.sh
+++ installation.sh
@@ -2,15 +2,7 @@
 
 SUDO_ID=$(id -u)
 
-if [ $SUDO_ID -eq 0]; then
-
-echo "Sudo user"
-exit 0
-fi
-
-dnf install mysql -y
-
-if[ $? -ne 0 ]; then
+if[ $SUDO_ID]; then
 echo "no acces"
 exit 1
 else 
@@ -18,3 +10,4 @@ echo "sudo user
 fi
 
 dnf install nginx -y
+
