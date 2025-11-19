5e2eeb3fdfe677b8f212288d58a39efd7a82a11f
suresh vaadapalli <surehs@gmail.com>
2025-11-19 18:54:55 +0530

    again varibles

--- variables.sh
+++ variables.sh
@@ -3,7 +3,7 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "PERSON1 : Hi how are you"
-echo "PERSON2 : fine, how aboout you"
-echo "PERSON1 : what are you ding"
-echo "PERSON2 : nothing"
+echo "$PERSON1 : Hi how are you"
+echo "$PERSON2 : fine, how aboout you"
+echo "$PERSON1 : what are you ding"
+echo "$PERSON2 : nothing"
