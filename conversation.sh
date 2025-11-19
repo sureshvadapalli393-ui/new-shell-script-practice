b46da400e2ff2318a6e7ca6bafe503f9dfbab5b5
suresh vaadapalli <surehs@gmail.com>
2025-11-19 18:36:44 +0530

    variable

--- conversation.sh
+++ conversation.sh
@@ -1,6 +1,9 @@
 #!bin/bash
 
-echo "Ramesh : : Hi suresh how are you "
-echo "suresh : : Hi ramesh how are you "
-echo "Ramesh :: what are you learning recently"
+VAR_NAME1=janu
+VAR_NAME2=satya
+
+echo "$VAR_NAME1 : : Hi $VAR_NAME2 how are you "
+echo "$VAR_NAME2 : : Hi $VAR_NAME1 how are you "
+echo "$VAR_NAME1 :: what are you learning recently"
 echo "i am learning linux"
