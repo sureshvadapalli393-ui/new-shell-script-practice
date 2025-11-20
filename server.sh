15456c3eca64292f59344a5c39a8f4f5ac002a29
suresh vaadapalli <surehs@gmail.com>
2025-11-20 13:09:42 +0530

    serverinstallation

--- server.sh
+++ server.sh
@@ -3,6 +3,5 @@
 aws ec2 run-instances \
   --image-id ami-09c813fb71547fc4f \
   --instance-type t2.micro \
-  --key-name firstKP \
   --security-group-ids sg-0c8311807a45cafaa \
   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyServer}]'
